5891a0fd69983701a0c851458a8e0076c6973c4e
DimaAlabsi <dema.yaser8@gmail.com>
2021-11-09 22:13:56 +0200

    edit

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -1,35 +1,35 @@
-// 'use strict';
+'use strict';
 
-// const userModel = require('./users');
-// const { Sequelize, DataTypes } = require('sequelize');
+const userModel = require('./users');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
-
-
-// let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     }
-//   }
-// } : {};
-
-// const sequelize = new Sequelize(DATABASE_URL,sequelizeOptions);
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
 
 
-// const clothesModel = require('./clothes/model');
-// const foodModel = require('./food/model.js');
-// const Collection = require('./data-collection');
+let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+} : {};
 
-// const food = foodModel(sequelize, DataTypes);
-// const clothes = clothesModel(sequelize, DataTypes);
+const sequelize = new Sequelize(DATABASE_URL,sequelizeOptions);
 
-// module.exports = {
-//   db: sequelize,
-//   users: userModel(sequelize, DataTypes),
-//   food: new Collection(food),
-//   clothes: new Collection(clothes),
-// }
+
+const clothesModel = require('./clothes/model');
+const foodModel = require('./food/model.js');
+const Collection = require('./data-collection');
+
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
+
+module.exports = {
+  db: sequelize,
+  users: userModel(sequelize, DataTypes),
+  food: new Collection(food),
+  clothes: new Collection(clothes),
+}
 
 
